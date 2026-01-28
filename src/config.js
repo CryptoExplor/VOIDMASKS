666ec10ae02f3d74e991ae41f02f407f33e8bffe
CryptoExplor <102873939+CryptoExplor@users.noreply.github.com>
2026-01-28 15:28:36 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const NETWORKS = {
         NETWORK: 'testnet',
         // Contract address is just the deployer address
         CONTRACT_ADDRESS: 'ST1HCWN2BWA7HKY61AVPC0EKRB4TH84TMV26A4VRZ',
-        CONTRACT_NAME: 'voidmasks',
+        CONTRACT_NAME: 'voidmasks1',
         STACKS_API: 'https://api.testnet.hiro.so',
         MINT_FEE: 0, // Free
         MIN_FEE_DISPLAY: 'Free'
@@ -70,5 +70,6 @@ export const utils = {
         return `#${tokenId.toString().padStart(4, '0')}`;
     }
 };
+
 
 
